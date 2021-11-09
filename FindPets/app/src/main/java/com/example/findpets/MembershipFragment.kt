e58aaaea948e62062a91703acce502c516178e03
ALEX VINICIUS COSTA DA SILVA <20272488@CFP127ALUNO.LOCAL>
2021-11-09 11:18:21 -0300

    tcc dia 09/11

--- FindPets/app/src/main/java/com/example/findpets/MembershipFragment.kt
+++ FindPets/app/src/main/java/com/example/findpets/MembershipFragment.kt
@@ -3,6 +3,7 @@ package com.example.findpets
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import com.example.findpets.databinding.FragmentFeedBinding
@@ -14,15 +15,21 @@ class MembershipFragment : Fragment() {
     lateinit var binding: FragmentMembershipBinding
 
 
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentMembershipBinding.inflate(inflater)
         return binding.root
     }
 
+    fun OnCreateOptionsMenu(menu: Menu?): Boolean {
 
+        val inflater = menuInflater
+
+        inflater.inflate(R.menu.membership_toolbar_menu, menu)
+        return true
+
+    }
 }
